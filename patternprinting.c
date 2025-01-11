68a95bee989e728d4bdc0d1a0380c1848fe35e36
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-01-11 11:23:08 +0530

    Update patternprinting.c

--- patternprinting.c
+++ patternprinting.c
@@ -300,3 +300,28 @@ int main()
 #****
 *****
 
+
+(14)
+#include <stdio.h>
+
+int main()
+{ int n;
+    printf("Enter no. of the row:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for( int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=n;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+(14)
+   ****
+  ****
+ ****
