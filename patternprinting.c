93db309f66f06b8c01d0ca2f1ae683246e38cfe3
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-01-12 18:52:48 +0530

    Update patternprinting.c

--- patternprinting.c
+++ patternprinting.c
@@ -512,9 +512,43 @@ int main()
         printf("\n"); 
     }
     return 0;
-
+(20)
     *
    ***
   *****
    ***
+    *
+
+
+
+(21)
+
+#include <stdio.h>
+
+int main()
+{ int n;
+    printf("Enter the no. of lines");
+    scanf("%d",&n);
+    int nst=n;
+    int nsp=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=nsp;j++){
+            printf(" ");
+             }
+             for(int k=1;k<=nst;k++){
+             printf("*");
+             }
+             nst--;
+             nsp++;
+             
+             printf("\n");
+    }
+    return 0;
+}
+ 
+(21)
+*****
+ ****
+  ***
+   **
     *
