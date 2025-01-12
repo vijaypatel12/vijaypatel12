66d8e0769acb504dc4e1b1d32aff3c8cc737cf2c
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-01-12 12:18:56 +0530

    Update patternprinting.c

--- patternprinting.c
+++ patternprinting.c
@@ -333,12 +333,39 @@ int main()
     ABC
    ABCD
 
+(16)  
+#include <stdio.h>
 
-
-
-
+int main()
+{ int n;
+    printf("Enter the no. of lines");
+     scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        int a=1;
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=2*i-1;k++){
+            char ch= (char)(a+64);
+            printf("%c",ch);
+            a++;
+            
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
 
 (16)
+   A
+  ABC
+ ABCDE
+
+
+
+
+(17)
 #include <stdio.h>
 
 int main()
@@ -361,7 +388,7 @@ int main()
     }
     return 0;
 }
-(16)
+(17)
    1
   123
  12345
