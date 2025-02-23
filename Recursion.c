5fe10cc2bed26fa1449849095ced0f1df7da582a
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-02-23 15:05:39 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -132,3 +132,25 @@ int main()
 
 
 (6) 1+2+3+4+5=15
+
+
+
+(7) raised to a power
+
+#include <stdio.h>
+int power(int a,int b){
+    if(b==0) return 1;
+    return a*power(a,b-1);
+}
+int main()
+{ int a;
+    printf("Enter base:");
+    scanf("%d",&a);
+    int b;
+    printf("Enter power:");
+    scanf("%d",&b);
+    int p=power(a,b);
+    printf("%d raised to a power %d %d",a,b,p);
+
+    return 0;
+}
