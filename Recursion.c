365ecefefb15b4a9e55ede8fd6926705245a2eab
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-02-23 12:45:02 +0530

    Update Recursion.c

--- Recursion.c
+++ Recursion.c
@@ -35,3 +35,27 @@ int main()
 
     return 0;
 }
+
+
+(3) 
+
+#include <stdio.h>
+void decreasing(int n){
+    if(n==0)return;
+    printf("%d\n",n);
+    decreasing(n-1);
+    return;
+}
+int main()
+{ int n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    decreasing(n);
+
+    return 0;
+}
+
+(3) 4
+    3 
+    2
+    1
