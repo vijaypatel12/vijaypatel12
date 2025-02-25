bda8ff9a0f1b41f9b744b47e7778e30aa6c235e0
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-02-25 09:29:26 +0530

    Update 2D Array.c

--- 2D Array.c
+++ 2D Array.c
@@ -19,3 +19,32 @@ int main()
 (1)
 1 2 
 3 4 
+
+
+
+(2)
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[2][2]={{1,2},{3,4}};
+ for(int i=0;i<2;i++){
+     for(int j=0;j<2;j++){
+         printf("enter the number ");
+         scanf("%d ",&arr[i][j]);
+     }
+     printf("\n");
+ }
+ {
+ int arr[2][2]={{1,2},{3,4}};
+ for(int i=0;i<2;i++){
+     for(int j=0;j<2;j++){
+         printf("%d ",arr[i][j]);
+     }
+     printf("\n");
+ }
+
+    return 0;
+ }
+}
