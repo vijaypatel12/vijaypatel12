774961ae9e9b7ae2234096ac8eb90e500cf77a77
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-02-25 11:59:30 +0530

    Update 2D Array.c

--- 2D Array.c
+++ 2D Array.c
@@ -77,3 +77,31 @@ for(int i=0;i<r;i++){
 }
     return 0;
 }
+
+
+
+
+
+hnn 
+(4) sum of two matrix A&B
+#include <stdio.h>
+
+int main()
+{
+int arr[2][2]={1,2,3,4};
+int brr[2][2]={5,6,7,8};
+printf("\n");
+ int result [2][2]; 
+for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++){
+        result[i][j]=arr[i][j]+brr[i][j];
+    }
+}
+for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++ ){
+        printf("%d ",result[i][j]);
+    }
+    printf("\n");
+}
+    return 0;
+}
