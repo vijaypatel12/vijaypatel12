b9ab8de56f78e658586c67c869a3076ece90a145
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-02-25 09:29:43 +0530

    Update 2D Array.c

--- 2D Array.c
+++ 2D Array.c
@@ -48,3 +48,32 @@ int main()
     return 0;
  }
 }
+
+
+(3)#include <stdio.h>
+
+int main()
+{
+    int r;
+    printf("Enter row;");
+    scanf("%d",&r);
+    int c;
+    printf("Enter column");
+    scanf("%d",&c);
+    int arr[r][c];
+   
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    } 
+    printf("\n");
+for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+        printf("%d ",arr[i][j]);
+        }
+     
+    printf("\n");
+}
+    return 0;
+}
