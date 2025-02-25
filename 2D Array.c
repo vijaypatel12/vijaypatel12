36531fde97fd16d3c532e7f43704887293037284
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-02-25 18:50:06 +0530

    Update 2D Array.c

--- 2D Array.c
+++ 2D Array.c
@@ -159,7 +159,33 @@ int c;
 
 (7) transpose of matrix
 
+#include <stdio.h>
 
+int main()
+
+{ int r;
+    printf("Enter the row");
+scanf("%d",&r);
+int c;
+    printf("Enter the column");
+scanf("%d",&c);
+printf("Enter all the elements ");
+int arr[r][c];
+for( int i=0;i<r;i++){
+    for(int j=0;j<c;j++){
+        scanf("%d",&arr[i][j]);
+    }
+    printf("\n");
+}
+for( int i=0;i<c;i++){
+    for(int j=0;j<r;j++){
+        printf("%d ",arr[j][i]);
+    }
+    printf("\n");
+    
+}
+    return 0;
+}
 
 
 
