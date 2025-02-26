21afc4cdfebc9a984c9cf89ff6e9bbea438d6ddd
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-02-26 10:10:29 +0530

    Update 2D Array.c

--- 2D Array.c
+++ 2D Array.c
@@ -154,6 +154,27 @@ int c;
 
 
 (6)maxino.
+ #include <stdio.h>
+
+int main()
+{
+    int arr[3][4]={ {1,0,1,1,},{0,1,0,1},{1,1,1,1}};
+  int maxcount=0;
+   int  Maxindex= -1;
+for(int i=0;i<3;i++){
+    int count=0;
+    for(int j=0;j<4;j++){
+        if(arr[i][j]==1) count++;  
+         }
+         if(maxcount<count){
+             maxcount=count;
+             Maxindex=i;
+         }
+         printf("\n");
+}
+printf("%d",Maxindex);
+    return 0;
+}
 
 
 
