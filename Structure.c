c9f706da4215e3fc61a2ef437eef4a935da8ad5d
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-03-08 23:51:13 +0530

    Update Structure.c

--- Structure.c
+++ Structure.c
@@ -33,3 +33,47 @@ int main()
 
     return 0;
 }
+
+
+
+
+(2)
+  
+#include <stdio.h>
+
+int main()
+{
+   typedef struct pokemon{
+       int hp;
+       int attack;
+       int speed;
+       char tier;
+   }pokemon;
+   
+   pokemon arr[3];
+   arr[0].attack=10;
+   arr[0].speed=20;
+   arr[0].hp=30;
+   arr[0].tier='A';
+
+ 
+   arr[1].attack=190;
+   arr[1].speed=201;
+   arr[1].hp=310;
+   arr[1].tier='B';
+   
+ 
+   arr[2].attack=110;
+   arr[2].speed=120;
+   arr[2].hp=130;
+   arr[2].tier='C';
+
+   for(int i=0;i<3;i++){
+        printf("%d\n",arr[i].attack);
+        printf("%d\n",arr[i].hp);
+        printf("%d\n",arr[i].speed);
+        printf("%c",arr[i].tier);
+        
+   }
+    return 0;
+}
