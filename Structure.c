0ab60e1893d00cd60fe5d076f827e9e26ddd4be5
vijaypatel12 <vijaygurjarppi@gmail.com>
2025-03-08 22:31:35 +0530

    Update Structure.c

--- Structure.c
+++ Structure.c
@@ -1,1 +1,35 @@
 
+#include <stdio.h>
+
+int main()
+{
+ struct Pokemon {
+     int attack;
+     int hp;
+     int speed;
+     char tier;
+ };
+ struct Pokemon pikachu;
+ 
+ pikachu.speed=50;
+ pikachu.hp=70;
+ pikachu.attack=90;
+ pikachu.tier=10;
+  
+  
+  struct Pokemon charizard;
+  charizard.attack=44;
+  charizard.hp=66;
+  charizard.speed=88;
+  charizard.tier=99;
+  
+  struct Pokemon mewtwo;
+  mewtwo.hp=10;
+  mewtwo.speed=20;
+  mewtwo.attack=30;
+  mewtwo.tier=40;
+  
+  printf("%d",pikachu.attack);
+
+    return 0;
+}
